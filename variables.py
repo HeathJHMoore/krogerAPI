1d5f098444e9ac948c77d5f8b0aca4bed518bba0
Heath JH Moore <heathjhmoore@gmail.com>
2019-10-13 13:18:04 -0500

    added variable skeleton

---
+++ variables.py
@@ -0,0 +1,19 @@
+API_Token_Endpoint = 'https://api.kroger.com/v1/connect/oauth2/token'
+API_Product_Endpoint = 'https://api.kroger.com/v1/products/'
+client_credentials = #my client id and client secret from Kroger
+mySQLUsername = #username for my python-anywhere mySQL database
+mySQLPassword = #password for my python-anywhere mySQL database
+locationIds = [
+  '02600567', #21st av Kroger
+  '02600511', #2615 Franklin Pike
+  '02600895', #2131 Abbott Martin Rd
+  '02600502', #800 Monroe St
+  '02600843', #4560 Harding Pike
+  '02600547', #5705 Charlotte Pike
+  '02600542', #61 E Thompson Ln
+  '02600880', #711 Gallatin Ave
+  '02600533', #3930 Clarksville Pike
+]
+productIds = [
+  '0003700094631'
+]
